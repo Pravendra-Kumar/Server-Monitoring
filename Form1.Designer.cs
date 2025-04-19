f074d709981b955b3fab978e46559be9fa344cb6
Pravendra Kumar <69899151+Pravendra-Kumar@users.noreply.github.com>
2025-04-19 22:58:35 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace DevicePer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblCPU = new System.Windows.Forms.Label();
+            this.lblRAM = new System.Windows.Forms.Label();
+            this.lblDisk = new System.Windows.Forms.Label();
+            this.lblNet = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.chkSave = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // lblCPU
+            // 
+            this.lblCPU.AutoSize = true;
+            this.lblCPU.Location = new System.Drawing.Point(2, 17);
+            this.lblCPU.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCPU.Name = "lblCPU";
+            this.lblCPU.Size = new System.Drawing.Size(29, 13);
+            this.lblCPU.TabIndex = 0;
+            this.lblCPU.Text = "CPU";
+            // 
+            // lblRAM
+            // 
+            this.lblRAM.AutoSize = true;
+            this.lblRAM.Location = new System.Drawing.Point(2, 47);
+            this.lblRAM.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblRAM.Name = "lblRAM";
+            this.lblRAM.Size = new System.Drawing.Size(31, 13);
+            this.lblRAM.TabIndex = 1;
+            this.lblRAM.Text = "RAM";
+            this.lblRAM.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblDisk
+            // 
+            this.lblDisk.AutoSize = true;
+            this.lblDisk.Location = new System.Drawing.Point(3, 79);
+            this.lblDisk.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblDisk.Name = "lblDisk";
+            this.lblDisk.Size = new System.Drawing.Size(28, 13);
+            this.lblDisk.TabIndex = 2;
+            this.lblDisk.Text = "Disk";
+            this.lblDisk.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lblNet
+            // 
+            this.lblNet.AutoSize = true;
+            this.lblNet.Location = new System.Drawing.Point(2, 117);
+            this.lblNet.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblNet.Name = "lblNet";
+            this.lblNet.Size = new System.Drawing.Size(24, 13);
+            this.lblNet.TabIndex = 3;
+            this.lblNet.Text = "Net";
+            this.lblNet.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // chkSave
+            // 
+            this.chkSave.AutoSize = true;
+            this.chkSave.Location = new System.Drawing.Point(402, 17);
+            this.chkSave.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.chkSave.Name = "chkSave";
+            this.chkSave.Size = new System.Drawing.Size(87, 17);
+            this.chkSave.TabIndex = 4;
+            this.chkSave.Text = "Save to CSV";
+            this.chkSave.UseVisualStyleBackColor = true;
+            this.chkSave.CheckedChanged += new System.EventHandler(this.chkSave_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 211);
+            this.Controls.Add(this.chkSave);
+            this.Controls.Add(this.lblNet);
+            this.Controls.Add(this.lblDisk);
+            this.Controls.Add(this.lblRAM);
+            this.Controls.Add(this.lblCPU);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "System Performance";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCPU;
+        private System.Windows.Forms.Label lblRAM;
+        private System.Windows.Forms.Label lblDisk;
+        private System.Windows.Forms.Label lblNet;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox chkSave;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using Microsoft.VisualBasic.Devices;
+
+namespace DevicePer
+{
+    public partial class Form1 : Form
+    {
+        PerformanceCounter cpuCounter;
+        PerformanceCounter ramCounter;
+        PerformanceCounter diskCounter;
+        PerformanceCounter netCounter;
+        ulong totalRAM;
+
+        string csvPath = @"D:\PRAVENDRA\SystemStatsLog.csv";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+            diskCounter = new PerformanceCounter("PhysicalDisk", "% Disk Time", "_Total");
+
+            string netInterface = new PerformanceCounterCategory("Network Interface").GetInstanceNames()[0];
+            netCounter = new PerformanceCounter("Network Interface", "Bytes Total/sec", netInterface);
+
+            totalRAM = new ComputerInfo().TotalPhysicalMemory;
+
+            if (!File.Exists(csvPath))
+            {
+                File.WriteAllText(csvPath, "Timestamp,CPU (%),RAM (%),Disk (%),Network (KB)\n");
+            }
+
+            timer1.Interval = 3000;
+            timer1.Start();
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            float cpu = GetCpuUsage();
+            float ram = GetRamUsagePercent();
+            float disk = GetDiskUsage();
+            float net = GetNetworkUsageInKBps();
+
+            lblCPU.Text = $"CPU: {cpu}%";
+            lblRAM.Text = $"RAM: {ram}%";
+            lblDisk.Text = $"Disk: {disk}%";
+            lblNet.Text = $"Net: {net}KB";
+
+            if (chkSave.Checked)
+            {
+                string line = $"{DateTime.Now},{cpu},{ram},{disk},{net}";
+                File.AppendAllText(csvPath, line + "\n");
+            }
+        }
+
+        private float GetCpuUsage()
+        {
+            cpuCounter.NextValue();
+            System.Threading.Thread.Sleep(500);
+            return (float)Math.Round(cpuCounter.NextValue(), 2);
+        }
+
+        private float GetRamUsagePercent()
+        {
+            float available = ramCounter.NextValue();
+            float used = ((totalRAM / (1024 * 1024)) - available) / (totalRAM / (1024 * 1024)) * 100;
+            return (float)Math.Round(used, 2);
+        }
+
+        private float GetDiskUsage()
+        {
+            diskCounter.NextValue();
+            System.Threading.Thread.Sleep(500);
+            return (float)Math.Round(diskCounter.NextValue(), 2);
+        }
+
+        private float GetNetworkUsageInKBps()
+        {
+            netCounter.NextValue();
+            System.Threading.Thread.Sleep(1000); // Wait for accurate reading
+            float usageMB = netCounter.NextValue() / 1024 ; // Convert Bytes to MB
+            return (float)Math.Round(usageMB, 2);
+        }
+
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chkSave_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
